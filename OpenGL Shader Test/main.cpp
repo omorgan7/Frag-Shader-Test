29b026d1a012a4e39603acad6383554dda07601b
Owen Morgan <o.morgan@bath.ac.uk>
2018-01-19 17:57:49 +0000

    Initial Commit

---
+++ OpenGL Shader Test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL Shader Test
+//
+//  Created by Owen Morgan on 19/01/2018.
+//  Copyright © 2018 Owen Morgan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
